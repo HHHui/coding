743db8fe3c888fba50df8d9498a639347f722f91
Zhang Hui <315765715@qq.com>
2018-04-09 11:12:38 +0800

    更新extension,支持修改action creator和state name

--- refactor/super-rename/src/extension.ts
+++ refactor/super-rename/src/extension.ts
@@ -3,7 +3,14 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import {Uri} from 'vscode'
-import diffs from './diff'
+
+import actionCreatorNameDiff from './actionCreatorNameDiff'
+import actionCreatorNameDiffMap from './actionCreatorNameDiffMap'
+import actionCreatorCall from './actionCreatorCall'
+
+import stateDiff from './stateDiff'
+import stateNeedtoChange from './collect'
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -12,37 +19,88 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "super-rename" is now active!');
     
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.sayHello', async() => {
-        let i = 0;
-        function step() {
-            if(i >= diffs.length) return;
-            let {old: o, new: n} = diffs[i];
-            vscode.commands.executeCommand('vscode.open', Uri.file(o.filePath))
-            .then(() => {
-                // -1,+1 可能是因为babylon parser和vscode的index从0或1开始的区别，这里随便凑了一下
-                vscode.commands.executeCommand('vscode.executeDocumentRenameProvider',
-                    Uri.file(o.filePath),
-                    new vscode.Position(o.loc.start.line - 1, o.loc.start.column),
-                n.name)
-                .then(edit => {
-                    if (!edit) {
-                        console.log('!edit return~');
-                        return false;
-                    }
-                    return vscode.workspace.applyEdit(edit);
-                })
+        // let i = 0;
+        // function step() {
+        //     if(i >= actionCreatorNameDiff.length) return;
+        //     let {old: o, new: n} = actionCreatorNameDiff[i];
+        //     vscode.commands.executeCommand('vscode.open', Uri.file(o.filePath))
+        //     .then(() => {
+        //         // -1,+1 可能是因为babylon parser和vscode的index从0或1开始的区别，这里随便凑了一下
+        //         vscode.commands.executeCommand('vscode.executeDocumentRenameProvider',
+        //             Uri.file(o.filePath),
+        //             new vscode.Position(o.loc.start.line - 1, o.loc.start.column),
+        //         n.name)
+        //         .then(edit => {
+        //             if (!edit) {
+        //                 console.log('!edit return~');
+        //                 return false;
+        //             }
+        //             return vscode.workspace.applyEdit(edit);
+        //         })
+        //         .then(() => {
+        //             setTimeout(() => {
+        //                 i++;
+        //                 step()
+        //             }, 200);
+        //         })
+        //     })
+        // }
+        // step();
+
+        // function changeActionCreatorCall(i:number){
+        //     if(i >= actionCreatorCall.length) return;
+        //     let state = actionCreatorCall[i];
+        //     vscode.commands.executeCommand('vscode.open', Uri.file(state.filepath))
+        //         .then(() => {
+        //             let editor = vscode.window.activeTextEditor;
+        //             if(editor){
+        //                 editor.edit(function cb(edit){
+        //                     edit.replace(new vscode.Range(
+        //                         new vscode.Position(state.loc.start.line - 1, state.loc.start.column),
+        //                         new vscode.Position(state.loc.end.line - 1, state.loc.end.column),
+        //                     ), actionCreatorNameDiffMap[state.name]);
+        //                 })
+        //             }
+        //         })
+        //         .then(() => {
+        //             setTimeout(() => {
+        //                 changeActionCreatorCall(++i)
+        //             }, 100);
+        //         })
+            
+        // }
+        // changeActionCreatorCall(0);
+        
+        let lastDate = new Date();
+        function changeStateName(i:number){
+            if(i >= stateNeedtoChange.length) return;
+            let state = stateNeedtoChange[i];
+            vscode.commands.executeCommand('vscode.open', Uri.file(state.filepath))
                 .then(() => {
-                    setTimeout(() => {
-                        i++;
-                        step()
-                    }, 200);
+                    let editor = vscode.window.activeTextEditor;
+                    if(editor){
+                        editor.edit(function cb(edit){
+                            console.log(`${state.name}:${stateDiff[state.name]}`)
+                            edit.replace(new vscode.Range(
+                                new vscode.Position(state.loc.start.line - 1, state.loc.start.column),
+                                new vscode.Position(state.loc.end.line - 1, state.loc.end.column),
+                            ), stateDiff[state.name]);
+                        })
+                        .then((value) => {
+                            console.log(`edit could be applied? ${value}`)
+                            setTimeout(() => {
+                                let current = new Date();
+                                console.log(current.getTime() - lastDate.getTime());
+                                lastDate = current;
+                                changeStateName(++i)
+                            }, 800);
+                        })
+                    }
                 })
-            })
+            
         }
-        step();
+        changeStateName(0);
     });
 
     context.subscriptions.push(disposable);
